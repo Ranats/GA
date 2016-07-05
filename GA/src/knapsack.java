60c32a78a3a1f159de2af25b0d5beb8a12c73144
Satoshi Coda <sopurani885@gmail.com>
2016-07-05 13:20:46 +0900

    Add files via upload

---
+++ GA/src/knapsack.java
@@ -0,0 +1,28 @@
+/**
+ * Created by 3bdi1230 on 2016/07/05.
+ */
+
+class Bag{
+
+}
+
+class kpGene extends Gene{
+
+    kpGene(int size){
+        super(size);
+        evaluate();
+    }
+
+    public void evaluate(){
+        score = 0;
+        int weight = 0;
+    }
+
+}
+
+public class knapsack {
+    public static void main(string[] args){
+
+    }
+
+}
